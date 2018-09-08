20adfd2c9031644e2fd05e1e483ecb30f0e16207
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-09-08 23:53:16 +0100

    Now param driven

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const efaAPI = require('./efaAPI');
 const app = express();
 
 app.get('/', (req, res) => (
-    efaAPI.stopDetailApi(10011588, 5).then(result => res.send(result))
-));
+    efaAPI.stopDetailApi(req.query.stop_id, req.query.limit))
+    .then(result => res.send(result))
+);
 
 app.listen(3000, function () {
   console.log('Belfast Glider API Server - listening on port 3000');
