aebab91a235b39b8ff58b506513fcbd10f04a9be
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-09-09 00:50:44 +0100

    First attempt at adding stop names

---
+++ data/stopNames.js
@@ -0,0 +1,81 @@
+
+const gOne = [
+    'McKinstry Road',
+    'Lagmore Avenue',
+    'Twinbrook Road',
+    'Colin Connect',
+    'Upper Dunmurry Lane',
+    'Glengoland',
+    'Woodbourne',
+    'Lenadoon Avenue',
+    'St. Genevieve\'s',
+    'Shaws Road',
+    'Slievegallion Drive',
+    'Casement Park',
+    'Friuthill Park',
+    'Beech Hall',
+    'Kennedy Centre',
+    'Falls Park',
+    'City Cemetery',
+    'Beechview Park',
+    'Beechmount',
+    'Children\'s Hospital',
+    'Royal Hospitals',
+    'Clonard',
+    'Twin Spires',
+    'Divis Tower',
+    'Millfield',
+    'Wellington Place',
+    'Chichester Street',
+    'Custom House Square',
+    'Waterfront',
+    'May Street (St George\'s Market)',
+    'May Street (City Hall)',
+    'College Square East',
+    'Lanyon Place',
+    'Short Strand',
+    'Templemore Avenue',
+    'Avoniel',
+    'Connswater',
+    'Holywood Arches',
+    'Beersbridge Road',
+    'Oakland Avenue',
+    'North Road',
+    'Ballyhackamore',
+    'Astoria Gardens',
+    'Greenwood Park',
+    'Knock Road',
+    'Cabin Hill',
+    'Stormont',
+    'Summerhill Avenue',
+    'Rosepark',
+    'Comber Road',
+    'Ulster Hospital',
+    'Cherryhill',
+    'Dundonald Park & Ride'
+];
+
+const gTwo = [
+    'Catalyst Inc.',
+    'Thompson Dry Dock',
+    'Titanic',
+    'Odyssey',
+    'Waterfront',
+    'May Street (St George\'s Market)',
+    'May Street (City Hall)',
+    'College Square East',
+    'Wellington Place',
+    'Chichester Street',
+    'Custom House Square'
+];
+
+const all = new Set([
+    ...gOne,
+    ...gTwo
+]);
+
+module.exports = {
+    all,
+    gOne,
+    gTwo
+};
