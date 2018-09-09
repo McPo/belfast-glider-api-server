303d092134e82cec26396970a9a2a2987444b9b6
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-09-09 02:06:42 +0100

    Made stop list on api

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const efaAPI = require('./efaAPI');
+const stops = require('./data/stops');
 
 const app = express();
 
-app.get('/', (req, res) => (
-    efaAPI.stopDetailApi(req.query.stop_id, req.query.limit))
-    .then(result => res.send(result))
-);
+app.get('/', (req, res) => {
+    if (req.query.id) {
+        efaAPI.stopDetailApi(req.query.id, req.query.limit)
+        .then(result => res.send(result));
+    } else {
+        res.send(stops);
+    }
+});
 
 app.listen(3000, function () {
   console.log('Belfast Glider API Server - listening on port 3000');
