6d0fd9aeae2ab7c36414aaa02e9c782196f3afb3
Emmet McPoland <emcpoland01@qub.ac.uk>
2018-09-09 02:36:02 +0100

    Implemented overview API

--- index.js
+++ index.js
@@ -5,13 +5,36 @@ const stops = require('./data/stops');
 const app = express();
 
 app.get('/list', (req, res) => (
-    res.send(stops)
+    res.send({
+        status: 'ok',
+        stops
+    })
 ));
 
 app.get('/stop/:id', (req, res) => (
     efaAPI.stopDetailApi(req.params.id, req.query.limit)
     .then(result => res.send(result))
 ));
+
+app.get('/overview', (req, res) => {
+    Promise.all(stops.map(s => efaAPI.stopDetailApi(s['id'], 1)))
+    .then(results => {
+        const stops = results.map(r => ({
+            name: r['stop'] ? r['stop']['name'] : null,
+            min_until: r['departures'] ? r['departures'][0]['min_until'] : null
+        }));
+        res.send({
+            status:'ok',
+            stops
+        });
+    })
+    .catch(err => {
+        res.send({
+            status:'error',
+            reason: err.toString()
+        });
+    })
+});
 
 app.listen(3000, function () {
   console.log('Belfast Glider API Server - listening on port 3000');
